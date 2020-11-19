ade1746390cdefd780e0c9472b3c60b30a9d5ef5
saihruday1997 <saihruday.08@gmail.com>
2020-11-19 17:25:01 +0530

    Routes and responses

--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@ const httpServer = http.createServer(handleServer);
 
 
 function handleServer(req, res) {
-  
+    if (req.url === "/welcome") {
+        res.statusCode - 200;
+        res.write("Welcome to Dominos!");
+        res.end();
+    } else if (req.url === "/contact") {
+        res.statusCode = 200;
+        res.json({phone: "18602100000", email: "guestcaredominos@jublfood.com"});
+        res.end();
+    }
+    else{
+        res.statusCode = 404;
+    }
 }
+
+httpServer.listen(8081);
 
 module.exports = httpServer;
