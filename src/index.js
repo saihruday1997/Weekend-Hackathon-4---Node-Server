1a56a857d8ede023b50db61d5c94d62f9701c130
saihruday1997 <saihruday.08@gmail.com>
2020-11-19 18:16:08 +0530

    Routes and responses changed code

--- src/index.js
+++ src/index.js
@@ -2,21 +2,17 @@ var http = require("http");
 
 const httpServer = http.createServer(handleServer);
 
-let jsonFile = JSON.stringify({
-    phone: '18602100000',
-    email: 'guestcaredominos@jublfood.com'
-});
-
 function handleServer(req, res) {
     if (req.url === "/welcome") {
-        res.statusCode = 200;
-        res.setHeader('content-type', 'text/plain');
+        res.writeHead(200 , {"Content-Type" : "text/plain"});
         res.write("Welcome to Dominos!");
         res.end();
     } else if (req.url === "/contact") {
-        res.statusCode = 200;
-        res.setHeader('content-type', 'application/json');
-        res.write(jsonFile);
+        res.writeHead(200, {"Content-Type" : "application/json"});
+        res.write(JSON.stringify({
+            phone: '18602100000',
+            email: 'guestcaredominos@jublfood.com'
+        }));
         res.end();
     } else {
         res.statusCode = 404;
